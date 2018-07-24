fce84f88022a13251dc8227c357b985326cb54ba
MegatronusPrime <nailahharvey2003@gmail.com>
2018-07-24 18:27:12 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,5 +1,5 @@
 def holiday_greeting (name1="Beyonce", name2="Jay-z")
-  puts "Happy Fourth of July, #{name1}! -Jay-z"
+  puts "Happy Fourth of July, #{name1}!-#{name2}"
 end
 
 holiday_greeting ("Mom")
